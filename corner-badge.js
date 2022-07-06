09989cbd8029d5498bff3bb49790f5f489cd31de
Mohamed Sahel <msahelme@gmail.com>
2022-07-06 15:07:32 +0100

    init

---
+++ corner-badge.js
@@ -0,0 +1,50 @@
+var SF__currentScript = document.currentScript || {}
+var SF__DOMReady = function (callback) {
+  document.readyState === 'interactive' ||
+  document.readyState === 'complete'
+    ? callback()
+    : document.addEventListener('DOMContentLoaded', callback)
+}
+SF__DOMReady(function () {
+  const el = document.createElement('div')
+
+  // Options
+  const url = SF__currentScript.getAttribute('url') || 'http://fetrah.org'
+  const title = SF__currentScript.getAttribute('title') || 'ادعم فطرة'
+  const position = SF__currentScript.getAttribute('position') || 'left'
+
+  // Set custom title
+  el.title = title
+
+  // Set custom URL
+  if (url) {
+    el.style.cursor = 'pointer'
+    el.target = '_blank'
+    el.addEventListener('click', function () {
+      window.open(url, '_blank')
+    })
+  }
+
+  // Set custom position (left, right)
+  if (position === 'right') {
+    el.style.right = '-80px'
+    el.style.bottom = '20px'
+    el.style.transform = 'rotate(135deg)'
+    el.style.background =
+      'linear-gradient(-360deg, #EF3E94 50%, #23BFE1 50%)'
+  } else {
+    el.style.left = '-80px'
+    el.style.bottom = '20px'
+    el.style.transform = 'rotate(45deg)'
+    el.style.background =
+      'linear-gradient(-180deg, #EF3E94 50%, #23BFE1 50%)'
+  }
+
+  el.style.width = '300px'
+  el.style.height = '84px'
+  el.style.position = 'fixed'
+  el.style.zIndex = '999'
+
+  el.setAttribute('id', 'we-stand-with-fetrah')
+  document.body.appendChild(el)
+})
