33ce64a1d38843bb1141cf9d1ae289d3830cc203
Mohamed Sahel <msahelme@gmail.com>
2022-07-07 22:20:40 +0100

    fix direction + support for smal viewports

--- corner-badge.js
+++ corner-badge.js
@@ -5,14 +5,28 @@ var SF__DOMReady = function (callback) {
     ? callback()
     : document.addEventListener('DOMContentLoaded', callback)
 }
+
 SF__DOMReady(function () {
+  const pinkColor = '#EF3E94'
+  const skyBlueColor = '#23BFE1'
   const el = document.createElement('div')
+  const viewport = window.innerWidth > 500 ? 'large' : 'small'
+  const sharedStyle = 'position: fixed; z-index: 99999999; cursor: pointer; '
+  const styles = {
+    right: {
+      large: `width: 260px; height: 74px;  right: -80px; bottom: 20px; transform: rotate(135deg); background: linear-gradient(-360deg, ${skyBlueColor} 50%, ${pinkColor} 50%)`,
+      small: `width: 200px; height: 64px;  right: -60px; bottom: 25px; transform: rotate(135deg); background: linear-gradient(-180deg, ${skyBlueColor} 50%, ${pinkColor} 50%)`,
+    },
+    left: {
+      large: `width: 260px; height: 74px;  left: -75px; bottom: 20px; transform: rotate(45deg); background: linear-gradient(-180deg, ${skyBlueColor} 50%, ${pinkColor} 50%)`,
+      small: `width: 200px; height: 64px;  left: -60px; bottom: 25px; transform: rotate(45deg); background: linear-gradient(-180deg, ${skyBlueColor} 50%, ${pinkColor} 50%)`,
+    },
+  }
 
   // Options
   const url =
-    SF__currentScript.getAttribute('url') ||
-    'https://www.facebook.com/fetrah.org/'
-  const title = SF__currentScript.getAttribute('title') || 'ادعم فطرة'
+    SF__currentScript.getAttribute('url') || 'http://www.fetrah.org/'
+  const title = SF__currentScript.getAttribute('title') || 'انشر فطرة'
   const position =
     SF__currentScript.getAttribute('position') || 'left'
 
@@ -29,25 +43,10 @@ SF__DOMReady(function () {
   }
 
   // Set custom position (left, right)
-  if (position === 'right') {
-    el.style.right = '-80px'
-    el.style.bottom = '20px'
-    el.style.transform = 'rotate(135deg)'
-    el.style.background =
-      'linear-gradient(-360deg, #EF3E94 50%, #23BFE1 50%)'
-  } else {
-    el.style.left = '-80px'
-    el.style.bottom = '20px'
-    el.style.transform = 'rotate(45deg)'
-    el.style.background =
-      'linear-gradient(-180deg, #EF3E94 50%, #23BFE1 50%)'
-  }
-
-  el.style.width = '300px'
-  el.style.height = '84px'
-  el.style.position = 'fixed'
-  el.style.zIndex = '999'
+  el.style =
+    sharedStyle + styles[position === 'right' ? 'right' : 'left'][viewport]
 
   el.setAttribute('id', 'we-stand-with-fetrah')
+  el.setAttribute('dir', 'ltr')
   document.body.appendChild(el)
 })
